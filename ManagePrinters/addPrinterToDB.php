f3b4f61431e9b6c1f73f030f09708ca3ac897087
Athena <bqh1402003@gmail.com>
2023-11-20 11:17:19 +0700

    addPrinter: update default setting desc

--- ManagePrinters/addPrinterToDB.php
+++ ManagePrinters/addPrinterToDB.php
@@ -8,6 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $campus = $_POST['campus'];
     $building = $_POST['building'];
 
+    //default setting when add is printer turn OFF
     $query = "INSERT INTO PRINTERS_LIST (PRINTERS_ID, PRINTERS_NAME, PRINTERS_DESC, PRINTERS_AVAI, PRINTERS_CAMPUSLOC, PRINTERS_BUILDINGLOC) VALUES (?, ?, ?, 'N', ?, ?)";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("sssss", $printerId, $printerName, $printerDesc, $campus, $building);
