8ca173feb2a71add66a7a63811985fc001ffad5a
xanhlaxalanh <113358436+xanhlaxalanh@users.noreply.github.com>
2023-11-22 13:00:40 +0700

    Login

---
+++ Login_with_Gmail/KT.php
@@ -0,0 +1,63 @@
+<?php
+    ob_start();
+    session_start();
+
+    require_once 'vendor/autoload.php';
+    require 'database.php';
+    require 'function.php';
+
+    $client = clientGoogle();
+    $service = new Google_Service_Oauth2($client);
+
+    if(isset($_GET['code'])){
+        $miss = $client->authenticate($_GET['code']);
+        if(isset($miss['error'])){ //Nếu nó lỗi 
+            header('Location: home.php');
+            exit();
+        }else{ //Nếu không xảy ra lỗi
+            $temp1 = $client->getAccessToken();
+            $user = $service->userinfo->get();
+
+            $_SESSION['user_info'] = [
+                'email' => $user->email,
+                'name' => $user->name,
+                'id' => $user->id,
+                'picture' => $user->picture,
+                'gender' => $user->gender,
+                'birthday' => $user->birthday,
+                'phone' => $user->phone,
+            ];
+
+            $email = mysqli_real_escape_string($conn, $user->email);
+            $check = "SELECT Role FROM Users WHERE Email = '$email'";
+            $result = mysqli_query($conn, $check);
+
+            if($result){ //Nếu có email xuất hiện thì đó là quản lý 
+                if(mysqli_num_rows($result) > 0){
+                    $_SESSION['session_id'] = uniqid('session_', true);
+                    header('Location: homeAfterLogin_Manage.php');
+                    exit();
+                }else{
+                    $email_gg = $user->email;
+                    $domain = "@hcmut.edu.vn";
+
+                    $position = strpos($email_gg, $domain);
+
+                    if($position !== false && $position == strlen($email_gg) - strlen($domain)){
+                        $_SESSION['session_id'] = uniqid('session_', true);
+                        header('Location: homeAfterLogin_User.php');
+                        exit();
+                    }
+                }
+            }else{
+                header('Location: home.php');
+                exit();
+            }
+            header('Location: home.php');
+            exit();
+        }
+    }else{
+        header('Location: home.php');
+        exit();
+    }
+?>
