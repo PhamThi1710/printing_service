e9392770344ba9508d4d9d6492a6f718f45123bd
Hà Thuỳ Dương <hathuyduong47@gmail.com>
2023-12-06 22:26:12 +0700

    Update ConfigureSystem Module

--- ConfigureSystem/DeleteAFileType.php
+++
@@ -1,9 +0,0 @@
-<?php
-    @include_once("../ConnectDB.php");
-
-    $File_Type = $_POST["File_Type"];
-
-    $sql = "DELETE FROM Accepted_File_Types WHERE File_Type = '$File_Type'";
-
-    $result = $conn->query($sql);
-?>
