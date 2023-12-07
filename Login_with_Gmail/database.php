35c591d87593ec7ae3a9a5ec3516d728759d4738
xanhlaxalanh <113358436+xanhlaxalanh@users.noreply.github.com>
2023-12-07 10:47:38 +0700

    update db

--- Login_with_Gmail/database.php
+++ Login_with_Gmail/database.php
@@ -2,11 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-<<<<<<< HEAD
     $database = "SSPS";
-=======
-    $database = "User";
->>>>>>> 9dab8ccedab132acc0b1777cd520808a93df7760
 
     $conn = mysqli_connect($servername, $username, $password, $database);
     if(!$conn) {
