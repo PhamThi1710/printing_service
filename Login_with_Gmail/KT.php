461da0febca68034f893e95a3d486a4a52c47c6d
xanhlaxalanh <113358436+xanhlaxalanh@users.noreply.github.com>
2023-12-07 10:35:46 +0700

    Merge branch 'main' of https://github.com/PhamThi1710/printing_service

--- Login_with_Gmail/KT.php
+++ Login_with_Gmail/KT.php
@@ -1,11 +1,16 @@
 <?php
-<<<<<<< HEAD
     ob_start();
     session_start();
 
     require_once 'vendor/autoload.php';
     require 'database.php';
     require 'function.php';
+
+    function insertUser($Fname, $Lname,$Email,$Role,$Sex,$Date_of_Birth,$Balance){
+
+        $insert='INSERT INTO `users`("Fname","Lname","Email","Role","Sex","Date_Of_Birth","Balance")VALUES(?, ?, ?, ?, ?, ?, ?)';
+    
+        mysqli_query($conn, $insert);    }
 
     $client = clientGoogle();
     $service = new Google_Service_Oauth2($client);
@@ -30,86 +35,66 @@
             ];
 
             $email = mysqli_real_escape_string($conn, $user->email);
-            $check = "SELECT Role FROM Users WHERE Email = '$email'";
+            $check = "SELECT Count(*) FROM Users WHERE Email = '$email'";
             $result = mysqli_query($conn, $check);
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+var_dump($email);
+echo $email;
+            if(!empty($data)){ //Nếu có email xuất hiện thì đó là quản lý 
+                $email_gg = $user->email;
+                $domain = "@hcmut.edu.vn";
 
-            if($result){ //Nếu có email xuất hiện thì đó là quản lý 
-                if(mysqli_num_rows($result) > 0){
-                    $_SESSION['session_id'] = uniqid('session_', true);
-                    header('Location: homeAfterLogin_Manage.php');
-                    exit();
-                }else{
-                    $email_gg = $user->email;
-                    $domain = "@hcmut.edu.vn";
+                $position = strpos($email_gg, $domain);
 
-                    $position = strpos($email_gg, $domain);
+                    if($position !== false){ // && $position == strlen($email_gg) - strlen($domain)){
+                        $_SESSION['student_id'] = uniqid('session_', true);
+$get = mysqli_query($conn,"select id,role from users where email = '$user->email' ");
+$getData = $get->fetch_all(MYSQLI_ASSOC);
+$ID = $getData[0]['id'];
+$Role = $getData[0]['Role'];
+$_SESSION['student_id'] = $ID;
+if($Role == "Student")
+{
+    header('Location: homeAfterLogin_User.php');
+}
+else{
+    header('Location: homeAfterLogin_Manage.php');
+}
+                    }
+                    else{
+                        echo ''
+                    }
 
-                    if($position !== false && $position == strlen($email_gg) - strlen($domain)){
-                        $_SESSION['session_id'] = uniqid('session_', true);
-                        header('Location: homeAfterLogin_User.php');
+                        $check = mysqli_query($conn,"SELECT COUNT(email) as emailcount FROM users WHERE email='$email_gg'");
+                        $email_count = $check->fetch_all(MYSQLI_ASSOC);
+
+                        if(!empty($email_count)) { // Nếu email tồn tại
+                            echo 'Welcome back '.$user->name.'! [<a href="'.$redirect_uri.'?logout=1">Log Out</a>]';
+                        } else { // Email không tồn tại trong cơ sở dữ liệu
+                            insertUser($_SESSION['user_info']['name'],'',$_SESSION['user_info']['email'],'Student',$_SESSION['user_info']['gender'],$_SESSION['user_info']['birthday'],'0'); 
+                        }
+
+                        //
                         exit();
                     }
+                if(mysqli_num_rows($result) > 0){
+                    $_SESSION['session_id'] = uniqid('session_', true);
+                    
+                    //
+                    exit();
+                }else{
+                    
                 }
             }else{
-                header('Location: home.php');
+
+                //header('Location: home.php');
                 exit();
             }
-            header('Location: home.php');
+            //header('Location: home.php');
             exit();
         }
     }else{
-        header('Location: home.php');
+        //header('Location: home.php');
         exit();
     }
-=======
-ob_start();
-session_start();
-
-require_once 'vendor/autoload.php';
-require 'database.php';
-require 'function.php';
-
-$client = clientGoogle();
-$service = new Google_Service_Oauth2($client);
-
-if (isset($_GET['code'])) {
-    $miss = $client->authenticate($_GET['code']);
-    if (isset($miss['error'])) { //Nếu nó lỗi 
-        header('Location: home.php');
-        exit();
-    } else { //Nếu không xảy ra lỗi
-        $temp1 = $client->getAccessToken();
-        $user = $service->userinfo->get();
-
-        $email = mysqli_real_escape_string($conn, $user->email);
-        $check = "SELECT Role_Type FROM User WHERE Email = '$email'";
-        $result = mysqli_query($conn, $check);
-
-        if ($result) {
-            if (mysqli_num_rows($result) > 0) {
-                $roleType = mysqli_fetch_assoc($result)['Role_Type'];
-                if ($roleType == 1) { //Nhân viên
-                    header('Location: homeAfterLogin_Manage.php');
-                    exit();
-                } else if ($roleType == 0) { //Người dùng, sinh viên, user
-                    header('Location: homeAfterLogin_User.php');
-                    exit();
-                }
-            }
-        }
-        if ($getUserResult && mysqli_num_rows($getUserResult) > 0) {
-            $userId = mysqli_fetch_assoc($result)['ID'];
-            $_SESSION['userId'] = $userId;
-        }
-        header('Location: home.php');
-        exit();
-    }
-} else {
-    header('Location: home.php');
-    exit();
-}
-
-
-
->>>>>>> 9dab8ccedab132acc0b1777cd520808a93df7760
 ?>
